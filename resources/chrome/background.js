5f91fd91388760348212e1a1ef980ac7c254a826
Maycon Michel Kruger <mig@certi.org.br>
2017-12-06 18:41:09 -0200

    [offline-online-chrome] -- Enable offline "projects" and "examples" submenus -- created resources folder -- moved translations and other resourses to resource folder -- clean up deploy script

--- resources/chrome/background.js
+++ resources/chrome/background.js
@@ -15,16 +15,45 @@ chrome.webRequest.onBeforeRequest.addListener(function (details) {
 				regex = /filename=\s*(.*?)\s*&approved/g;
 				result = regex.exec(requested_url);
 				result = result[1];
-				var file = result.replace(/%2F/i,"/");	
+				file = result.replace(/%2F/i,"/");	
+		}
+		if(requested_url.indexOf("api/md/microbit/projects/") > -1) {    //extract language and file name from url
+				var regex = /lang=\s*(.*?)\s*&live/g;
+				var result = regex.exec(requested_url);
+				lang = result[1];
+				regex = /projects\s*(.*?)\s*\?/g;
+				result = regex.exec(requested_url);
+				file = result[1];
+				console.log(file);
+		}
+		if(requested_url.indexOf("api/md/microbit/examples/") > -1) {    //extract language and file name from url
+				var regex = /lang=\s*(.*?)\s*&live/g;
+				var result = regex.exec(requested_url);
+				lang = result[1];
+				regex = /examples\s*(.*?)\s*\?/g;
+				result = regex.exec(requested_url);
+				file = result[1];
 		}
 	}
 	
 	if(details.url.indexOf("https://www.pxt.io") > -1) { 
 		if(!online) {
 			redirect_url = requested_url.replace("https://www.pxt.io","chrome-extension://ngbgjifibhpeaiomjmfhnegegokbmlgj");//browser is offline, load resources from local server
+			if(details.url.indexOf("www.pxt.io/api/md/microbit/projects?targetVersion=0.0.0&lang=pt-BR&live=1") > -1) {
+				redirect_url = "chrome-extension://ngbgjifibhpeaiomjmfhnegegokbmlgj/api/md/microbit/projectslist";
+			}
+			if(details.url.indexOf("www.pxt.io/api/md/microbit/examples?targetVersion=0.0.0&lang=pt-BR&live=1") > -1) {
+				redirect_url = "chrome-extension://ngbgjifibhpeaiomjmfhnegegokbmlgj/api/md/microbit/exampleslist";
+			}
+			if(requested_url.indexOf("www.pxt.io/api/md/microbit/projects/") > -1) { 
+				redirect_url = "chrome-extension://ngbgjifibhpeaiomjmfhnegegokbmlgj/api/md/microbit/projects/" + lang + file;
+			}
+			if(requested_url.indexOf("www.pxt.io/api/md/microbit/examples/") > -1) { 
+				redirect_url = "chrome-extension://ngbgjifibhpeaiomjmfhnegegokbmlgj/api/md/microbit/examples/" + lang + file;
+			}
 		}
 	}
-	
+
 	if(details.url.indexOf("https://www.pxt.io/api/config/microbit/targetconfig") > -1) {
 		redirect_url = "chrome-extension://ngbgjifibhpeaiomjmfhnegegokbmlgj/api/config/microbit/targetconfig";
 	}
--- tools/deploy.sh
+++ tools/deploy.sh
@@ -4,7 +4,9 @@ cd ..
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #absolute root dir
 
 PACKAGED=$DIR"/deploy/microbit"      #static html packaged abs path
-PEMFILE=$DIR"/tools/microbit.pem"    #pem file abs path
+RESOURCES=$DIR"/resources"
+PEMFILE=$RESOURCES"/chrome/microbit.pem"    #pem file abs path
+
 
 cd tools
 rm -rf ../deploy
@@ -16,9 +18,6 @@ cp -r ../built/packaged ../deploy/microbit
 cd jx
 ./run.sh
 cd ..
-cp manifest.json ../deploy/microbit
-cp background.js ../deploy/microbit
-cp icon-microbit-128.png ../deploy/microbit
 cp jx/microbit.exe ../deploy/microbit
 cp jx/microbit.bat ../deploy
 mkdir -p ../deploy/microbit/api/compile
@@ -29,28 +28,24 @@ cd ../deploy/microbit/api/compile
 sed -i '1s;^;{"enums":[],"functions":[],"hex":";' 0662709fa031556725d5759589cee8061b26701a654d387f175c459b186d0d71.json
 sed -i '$s/$/\\r\\n"}/' 0662709fa031556725d5759589cee8061b26701a654d387f175c459b186d0d71.json
 sed -i ':a;N;$!ba;s/\n/\\r\\n/g' 0662709fa031556725d5759589cee8061b26701a654d387f175c459b186d0d71.json
-cd ../../../../tools
+cd $DIR/tools
 
 #copy translations to deploy folder
-mkdir -p ../deploy/microbit/api/translations/pt-BR/microbit
-cp ../translations/pt-BR/*-strings.json ../deploy/microbit/api/translations/pt-BR/microbit
-cp ../translations/pt-BR/strings.json ../deploy/microbit/api/translations/pt-BR/
-cp ../translations/pt-BR/website.json ../deploy/microbit/api/translations/pt-BR/
-
-mkdir -p ../deploy/microbit/api/translations/es-ES/microbit
-cp ../translations/es-ES/*-strings.json ../deploy/microbit/api/translations/es-ES/microbit
-cp ../translations/es-ES/strings.json ../deploy/microbit/api/translations/es-ES/
-cp ../translations/es-ES/website.json ../deploy/microbit/api/translations/es-ES/
-
+cp -r $RESOURCES/api/translations ../deploy/microbit/api/
 
 #copy targetconfig to deploy folder
-
 mkdir -p ../deploy/microbit/api/config/microbit
-cp ../translations/targetconfig.json ../deploy/microbit/api/config/microbit/
+cp $RESOURCES/api/config/microbit/* ../deploy/microbit/api/config/microbit/
 
 #copy some files to ensure compatibility with chrome extension
-cp ../deploy/microbit/api/config/microbit/targetconfig.json ../deploy/microbit/api/config/microbit/targetconfig
 cp ../deploy/microbit/api/compile/0662709fa031556725d5759589cee8061b26701a654d387f175c459b186d0d71.json ../deploy/microbit/api/compile/0662709fa031556725d5759589cee8061b26701a654d387f175c459b186d0d71
+
+#copy chrome extension required files
+cp $RESOURCES/chrome/* ../deploy/microbit/
+
+#copy aditional resources for offline compatibility
+cp -r $RESOURCES/api/md  		 ../deploy/microbit/api/
+cp $RESOURCES/api/clientconfig ../deploy/microbit/api
 
 #pack deploy folder into .crx file
 chrome.exe --pack-extension=$PACKAGED --pack-extension-key=$PEMFILE
