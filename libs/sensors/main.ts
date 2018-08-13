a726c112524cfaf694605d7b1813d79a302a13e3
ltn <ltn@certi.org.br>
2018-08-13 15:05:32 -0300

    Add getBrightness block on Sensors package
    * Include new translations to getBrightness block

--- libs/sensors/main.ts
+++ libs/sensors/main.ts
@@ -16,6 +16,17 @@ namespace sensors {
     }
 
     /**
+     * "Get the brightness of a NeoPixel strip from 0 (off) to 50 (full bright)."
+     * @param strip a NeoPixel strip.
+     */
+    //% blockId="get_neopixel_brightness" block="%x=variables_get|get brightness" blockGap=8
+    //% weight=59
+    //% advanced=true
+    export function getBrightness(strip: neopixel.Strip): number {
+        return strip.brightness;
+    }
+
+    /**
      * Create block that receives a direction value (1 for clockwise and -1 counter-clockwise) and a speed value from 0 to 100%
      * @param direction turning direction, eg:1
      * @param value speed value from 0 to 100%, eg:100
@@ -24,18 +35,18 @@ namespace sensors {
     //% value.min=0 value.max=100
     //% direction.min=-1 direction.max=1
     export function servoWritePinContinuos(direction: number, value: number): void {
-        if(value > 100){
+        if (value > 100){
             value = 100;
         }
-        if(value < 0){
+        if (value < 0){
             value = 0;
         }
-        if(direction != 0 && value != 0){
+        if (direction != 0 && value != 0){
             direction=direction/Math.abs(direction);
             value = ((value * 90) / 100);
             value = 90 + (value * direction);
             pins.servoWritePin(AnalogPin.P0, value);
-        }else{
+        } else {
             pins.digitalReadPin(DigitalPin.P0);
             pins.pulseIn(DigitalPin.P0, PulseValue.Low);
         }
