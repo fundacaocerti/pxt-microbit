8b9802e1943d28ffd3eab5e85cef9ef7d83d491a
ltn <ltn@certi.org.br>
2018-08-20 18:28:04 -0300

    Remove unnecessary redirect
    * We don't use Grove as a default package anymore, so this redirect is no longer necessary.

--- resources/replacements/change-external-packages-versions.js
+++ resources/replacements/change-external-packages-versions.js
@@ -1,8 +1,5 @@
             var packageVersion = "*";
-            if (scr.name == "Grove") {
-                packageVersion = "github:seeed-studio/pxt-grove#v0.1.0";
-            }
-            else if (scr.name == "weatherbit") {
+            if (scr.name == "weatherbit") {
                 packageVersion = "github:sparkfun/pxt-weather-bit#v0.0.10";
             }
             addDepIfNoConflict(scr, packageVersion)
