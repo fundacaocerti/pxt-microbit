94641f800374e4ac401157659c9178fb74481f8b
Elisa de Freitas Kuhlkamp <efk@certi.org.br>
2018-09-03 09:47:11 -0300

    Add package version for sensors package

--- resources/replacements/change-external-packages-versions.js
+++ resources/replacements/change-external-packages-versions.js
@@ -1,5 +1,8 @@
             var packageVersion = "*";
-            if (scr.name == "weatherbit") {
+            if (scr.name == "sensors") {
+                packageVersion = "github:fundacaocerti/pxt-sensors#v0.1";
+            }
+            else if (scr.name == "weatherbit") {
                 packageVersion = "github:sparkfun/pxt-weather-bit#v0.0.10";
             }
             addDepIfNoConflict(scr, packageVersion)
